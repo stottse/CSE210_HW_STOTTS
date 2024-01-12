34f52ee6f99b884b90957e47dedbdf33cf8a20f8
Ethan Stotts <sto17018@byui.edu>
2024-01-12 15:21:48 -0700

    Finished Prep #2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,45 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Please enter your grade:");
+        string grade = Console.ReadLine();
+        int newGrade = int.Parse(grade);
+        string letter = "";
+        if (newGrade >= 90)
+        {
+            letter = "A";
+            //Console.WriteLine("You got an A");
+        }
+        else if (newGrade >= 80)
+        {
+            letter = "B";
+            //Console.WriteLine("You got a B");
+        }
+        else if (newGrade >= 70)
+        {
+            letter = "C";
+            //Console.WriteLine("You got a C");
+        }
+        else if (newGrade >= 60)
+        {
+            letter = "D";
+            //Console.WriteLine("You got a D");
+        }
+        else
+        {
+            letter = "F";
+            //Console.WriteLine("You got a F");
+        }
+        Console.WriteLine($"You got a {letter}");
+
+        if (newGrade >= 70)
+        {
+            Console.WriteLine("You Passed");
+        }
+        else
+        {
+            Console.WriteLine("Sorry you didn't pass. Try again.");
+        }
+
     }
 }
