94ad35584ba7402af41c41c391771e27db723448
Ethan Stotts <sto17018@byui.edu>
2024-01-18 20:15:57 -0700

    Finished prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string UserName = PromptUserName();
+        int Usernumber = PromptUserNumber();
+        int SquaredNumber = SqaureNumber(Usernumber);
+        DisplayResult(SquaredNumber,UserName);
+
+    }
+    static void DisplayWelcome(){
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName(){
+        Console.WriteLine("Please enter your name");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber(){
+        Console.WriteLine("Please enter your favorite integer number");
+        string input = Console.ReadLine();
+        int number = int.Parse(input);
+        return number;
+    }
+    static int SqaureNumber(int square){
+        int squared = square * square;
+        return squared;
+    }
+    static void DisplayResult(int squared, string name){
+        Console.WriteLine($"{name}, your number squared is {squared}");
+
     }
 }
