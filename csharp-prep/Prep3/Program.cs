4b88a91e752bf2fc62272b9f4c1e732706119485
Ethan Stotts <sto17018@byui.edu>
2024-01-18 18:20:53 -0700

    Finished Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("I'm thinking of a magic number between 1 and 100.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        int guess = 0;
+
+
+
+
+        while (number != guess){
+            Console.WriteLine("Please guess the magic number");
+            string newGuess = Console.ReadLine();
+            guess = int.Parse(newGuess);
+            if (number > guess){
+                Console.WriteLine("Your guess is too low");
+            }
+            else if (number < guess){
+                Console.WriteLine("Your guess is too high");
+            }
+            else{
+                Console.WriteLine("You guessed correctly!");
+            }
+            
+        }
+        
+
     }
 }
