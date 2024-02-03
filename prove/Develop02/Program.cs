ad6fbb8b5c9f9653bfaccef7da6dcad9b886b46f
Ethan Stotts <sto17018@byui.edu>
2024-02-03 13:46:59 -0700

    Just saving work so far. worked on options

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("Welcome to your journal!");
+        Console.WriteLine("Please select make a selection from the options below:");
+        Console.WriteLine("1. Who was the most interesting person I interacted with today?");
+        Console.WriteLine("2. What was the best part of my day?");
+        Console.WriteLine("3. How did I see the hand of the Lord in my day?");
+        Console.WriteLine("4. What was the strongest emotion I felt today?");
+        Console.WriteLine("5. If I had one thing I could do over today, what would it be?");
+        Console.WriteLine("6. Something new I learned today.");
+        Console.WriteLine("7. Someone new I met today.");
+        Console.WriteLine("8. A spiritual experience that happened today.");
+        Console.WriteLine("9. The best part of the day.");
+        Console.WriteLine("10. The best food I had today.");
+        string Option_Selected = Console.ReadLine();
+
+        if (Option_Selected == "1")
+        {
+
+        }
+
+    
     }
 }
