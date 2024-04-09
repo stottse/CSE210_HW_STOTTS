9a7726f2b6f7255403f3eee8bbe5accb773eb3de
Ethan Stotts <sto17018@byui.edu>
2024-04-08 22:43:54 -0600

    Making more progress on foundation 3

--- final/Foundation3/Event.cs
+++ final/Foundation3/Event.cs
@@ -7,13 +7,19 @@ public class Event
     protected string _time;
     protected Address _address;
 
-    public Event(string eventTitle, string description, string date, string time, Address address)
+    //I am going to try and go into the child class and from there load in the info to the parent class
+    protected string _eventDetails;
+    protected string _eventType;
+
+    public Event(string eventTitle, string description, string date, string time, Address address, string eventDetails, string eventType)
     {
         _eventTitle = eventTitle;
         _description = description;
         _date = date;
         _time = time;
         _address = address;
+        _eventDetails = eventDetails;
+        _eventType = eventType;
 
     }
 
@@ -22,9 +28,15 @@ public class Event
         return $"Title: {_eventTitle}\nDescription: {_description}\nDate: {_date}\nAddress: {_address}";
     }
 
-    public string GetFullDetails()
+    public virtual string GetFullDetails()
     {
-        return
+        return $"Event Type: {_eventType}\nEvent Specifics: {_eventDetails}\n" + GetStandardDetails();
+
+    }
+
+    public string GetShortDescription()
+    {
+        return $"Event Type: {_eventType}\nTitle: {_eventTitle}\nDate: {_date}";
 
     }
 
